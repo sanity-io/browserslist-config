4f3921ae2d8f52c9d54c1f1efb3dda5922a08904
Cody Olsen <stipsan@gmail.com>
2024-02-07 23:25:43 +0100

    fix: no longer less than 80% coverage in China regions

--- test.mjs
+++ test.mjs
@@ -5,8 +5,5 @@ import config from '@sanity/browserslist-config'
 
 test('the config lints without warnings', () => {
   const result = lint(config)
-  // We only expect 1 warning, about coverage in the `China` regions
-  assert.strictEqual(result.length, 1)
-  assert.strictEqual(result[0].id, 'countryWasIgnored')
-  assert.strictEqual(result[0].message, 'Less than 80% coverage in `China` regions')
+  assert.deepEqual(result, [])
 })
