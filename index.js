e7a474d18aed117fa5e56cf6c0af618d436ab9ec
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-12-19 00:38:01 +0100

    fix: improve bundlesize by using native object rest spread syntax
    Signed-off-by: Cody Olsen <81981+stipsan@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// https://browsersl.ist/#q=%3E+0.3%25+and+not+dead+and+fully+supports+es6-module+and+fully+supports+es6-module-dynamic-import%2C%0Amaintained+node+versions
+// https://browsersl.ist/#q=%3E+0.5%25+and+not+dead+and+fully+supports+es6-module+and+fully+supports+es6-module-dynamic-import%2C%0Amaintained+node+versions
 
 module.exports = [
   // We try to support most markets while also scoping it to browsers that fully supports ESM, and can natively call import()
   // as much of our products and tooling rely on ESM and dynamic imports being available to the runtime.
-  '> 0.3% and not dead and fully supports es6-module and fully supports es6-module-dynamic-import',
+  '> 0.5% and not dead and fully supports es6-module and fully supports es6-module-dynamic-import',
   // Most of our npm libraries also need to work in Node.js, at least to the LTS versions.
   'maintained node versions',
 ]
